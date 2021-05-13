bccc81854ef759b20714727436f5e6b76c0dcd02
ValterZoldhegyi <37004650+ValterZoldhegyi@users.noreply.github.com>
2021-05-13 17:35:33 +0200

    Update utility.py
    - Ported from Python 2.x to Python 3.6
    - Added: Calculate reprojection error

--- ILCC/utility.py
+++ ILCC/utility.py
@@ -5,17 +5,17 @@ Created on 3/20/2017 8:58 57PM Wang Weimin
 @author: wangwm
 '''
 import os
-from pcd_corners_est import exact_full_marker_data
+from .pcd_corners_est import exact_full_marker_data
 import numpy as np
-from pcd_corners_est import generate_grid_coords
+from .pcd_corners_est import generate_grid_coords
 import matplotlib.pyplot as plt
 import matplotlib
 import vtk
-import config
+from . import config
 from ast import literal_eval as make_tuple
-import cPickle
+import pickle as cPickle
 import cv2
-from LM_opt import xyz2angle, voxel2pixel
+from .LM_opt import xyz2angle, voxel2pixel
 import transforms3d
 from matplotlib.pyplot import cm
 import ast
@@ -29,7 +29,7 @@ marker_size = make_tuple(params["pattern_size"])
 (H, W) = make_tuple(params['image_res'])
 
 matplotlib.rcParams['text.usetex'] = True
-matplotlib.rcParams['text.latex.unicode'] = True
+                                                
 plt.style.use("ggplot")
 
 axis_font = {'fontname': 'Arial', 'size': '35'}
@@ -53,7 +53,7 @@ def draw_one_grd_vtk(ls):  # arr:[a,b,c,d],a:orig, b, point1, c,point 2, d,color
     # color_tup = np.random.randint(1, 255, 3)
 
     color.SetNumberOfTuples(source.GetOutput().GetNumberOfCells())
-    for i in xrange(source.GetOutput().GetNumberOfCells()):
+    for i in range(source.GetOutput().GetNumberOfCells()):
         color_tup = np.array([255, 255, 255]) * ls[3]
         color.InsertTuple(i, color_tup)
 
@@ -80,39 +80,39 @@ def gen_color_tup_for_vis(color_style="intens_rg", xyzi_arr=None):
     color_ls = []
     if color_style == "intens_rg":
         tmp = (xyzi_arr[:, params['intensity_col_ind']] - a) / (b - a) * 255
-        for k in xrange(xyzi_arr.shape[0]):
+        for k in range(xyzi_arr.shape[0]):
             rgb_tuple = np.array([tmp[k], 0, 255 - xyzi_arr[k, params['intensity_col_ind']]]).astype(np.int32)
             color_ls.append(rgb_tuple)
         return color_ls
     elif color_style == "intens":
         tmp = (xyzi_arr[:, params['intensity_col_ind']] - a) / (b - a) * 255
-        for k in xrange(xyzi_arr.shape[0]):
+        for k in range(xyzi_arr.shape[0]):
             rgb_tuple = np.repeat(tmp[k], 3).astype(np.int32)
             color_ls.append(rgb_tuple)
         return color_ls
     elif color_style == "autumn":
         tmp = (xyzi_arr[:, params['intensity_col_ind']] - a).astype(np.float32) / (b - a)
-        for k in xrange(xyzi_arr.shape[0]):
+        for k in range(xyzi_arr.shape[0]):
             rgb_tuple = (np.array(plt.cm.autumn(1 - tmp[k]))[:3] * 255).astype(np.int32)
             color_ls.append(rgb_tuple)
         return color_ls
     elif color_style == "cool":
         tmp = (xyzi_arr[:, params['intensity_col_ind']] - a).astype(np.float32) / (b - a)
-        for k in xrange(xyzi_arr.shape[0]):
+        for k in range(xyzi_arr.shape[0]):
             rgb_tuple = (np.array(plt.cm.cool(tmp[k]))[:3] * 255).astype(np.int32)
             color_ls.append(rgb_tuple)
         return color_ls
     elif color_style == "monochrome":
         # color = (np.random.randint(0, 255, 3)).tolist()
         color = [46, 204, 113]
-        for k in xrange(xyzi_arr.shape[0]):
+        for k in range(xyzi_arr.shape[0]):
             color_ls.append(color)
         return color_ls
     elif color_style == "by_height":
         low_height = xyzi_arr[:, 2].min()
         high_height = xyzi_arr[:, 2].max()
         tmp = 0.0 + 0.7 * (xyzi_arr[:, 2] - low_height) / (high_height - low_height)
-        for k in xrange(xyzi_arr.shape[0]):
+        for k in range(xyzi_arr.shape[0]):
             rgb_tuple = (np.array(plt.cm.hsv(tmp[k]))[:3] * 255).astype(np.int32)
             color_ls.append(rgb_tuple)
         return color_ls
@@ -131,7 +131,7 @@ def vis_3D_points(full_lidar_arr, color_style="intens_rg"):
 
     tuple_ls = gen_color_tup_for_vis(color_style, xyzi_arr=full_lidar_arr)
 
-    for k in xrange(all_rows):
+    for k in range(all_rows):
         point = full_lidar_arr[k, :3]
         id = Points.InsertNextPoint(point[0], point[1], point[2])
         Vertices.InsertNextCell(1)
@@ -171,7 +171,7 @@ def vis_pcd_color_arr(array_data, color_arr=[46, 204, 113]):
     Points = vtk.vtkPoints()
     Vertices = vtk.vtkCellArray()
 
-    for k in xrange(all_rows):
+    for k in range(all_rows):
         point = array_data[k, :]
         id = Points.InsertNextPoint(point[0], point[1], point[2])
         Vertices.InsertNextCell(1)
@@ -232,7 +232,7 @@ def vis_with_renderer(renderer):
             else:
                 writer.SetInputData(w2if.GetOutput())
             writer.Write()
-            print "screenshot saved"
+            print ("screenshot saved")
 
     style = vtk.vtkInteractorStyleSwitch()
     renderWindowInteractor.SetInteractorStyle(style)
@@ -287,7 +287,7 @@ def remove_occlusion_of_chessboard(pcd_arr, corners_in_pcd_arr):
         inds = np.where(pcd_arr[:, 2] > 0)
         pcd_ls = pcd_arr[inds].tolist()
         pix_ls = np.array(pix_ls)[inds].tolist()
-        print "before removal: ", len(pcd_ls)
+        print ("before removal: ", len(pcd_ls))
         proj_pts = (pcd_to_pix / pcd_to_pix[:, 2].reshape(-1, 1))[:, :2].astype(np.int16)
         bound_on_image = np.fliplr(proj_pts)
         # bound_on_image = proj_pts
@@ -298,7 +298,7 @@ def remove_occlusion_of_chessboard(pcd_arr, corners_in_pcd_arr):
 
     polygon_path = mplPath.Path(bound_on_image.tolist())
 
-    for i in xrange(len(pcd_ls)):
+    for i in range(len(pcd_ls)):
         pix = list(reversed(pix_ls[i]))
         # print pix
         if polygon_path.contains_point(pix):
@@ -359,11 +359,11 @@ def vis_segments_only_chessboard_color(ind=1):
     seg_list = os.listdir(seg_folder)
     chessboard_file_name = \
         cPickle.load(open(os.path.join(params['base_dir'], "output/pcd_seg/") + str(ind).zfill(
-            params["file_name_digits"]) + "_pcd_result.pkl", "r"))[
+            params["file_name_digits"]) + "_pcd_result.pkl", "rb"))[
             -1].split("/")[-1]
     for seg in seg_list:
         if seg.split(".")[-1] == "txt":
-            if seg == chessboard_file_name:
+            if seg in chessboard_file_name:
                 color_tup = np.array([0, 255, 0])
             else:
                 color_tup = np.array([0, 0, 0])
@@ -402,7 +402,7 @@ def vis_all_markers(ls=[1]):
                                                params["file_name_digits"]) + "_pcd_result.pkl")
             csv_path = os.path.join(params['base_dir'], "pcd/" + str(i).zfill(params["file_name_digits"]) + ".csv")
 
-            with open(os.path.abspath(pcd_result_file), "r") as f:
+            with open(os.path.abspath(pcd_result_file), "rb") as f:
                 pcd_result_ls = cPickle.load(f)
             assert pcd_result_ls is not None
 
@@ -425,7 +425,7 @@ def vis_all_markers(ls=[1]):
                     color_style="intens_rg")
             ren.AddActor(actor2)
         except:
-            print i, "-th pcd corners are not found!"
+            print (i, "-th pcd corners are not found!")
             continue
 
     transform2 = vtk.vtkTransform()
@@ -477,7 +477,7 @@ def vis_all_markers(ls=[1]):
             writer.SetFileName("screenshot.png")
             writer.SetInputData(w2if.GetOutput())
             writer.Write()
-            print "screenshot saved"
+            print ("screenshot saved")
 
         # save to pdf
         if iren.GetKeyCode() == "s":
@@ -520,7 +520,7 @@ def vis_ested_pcd_corners(ind=1):
 
     grid_coords = generate_grid_coords()
 
-    with open(os.path.abspath(pcd_result_file), "r") as f:
+    with open(os.path.abspath(pcd_result_file), "rb") as f:
         pcd_result_ls = cPickle.load(f)
     assert pcd_result_ls is not None
 
@@ -532,7 +532,7 @@ def vis_ested_pcd_corners(ind=1):
     trans_grid_ls = []
     for coords in grid_coords:
         args = [[coord, rot1, rot2, t1, t2] for coord in coords[:3]]
-        trans_coords = map(transform_grid, args)
+        trans_coords = list(map(transform_grid, args))
         trans_coords.append(coords[3])
         trans_grid_ls.append(trans_coords)
 
@@ -541,7 +541,7 @@ def vis_ested_pcd_corners(ind=1):
     ren.SetBackground(0.90196079, 0.96078432, 0.59607846)
     # ren.SetBackground(1., 1., 1.)
 
-    for i in xrange(len(trans_grid_ls)):
+    for i in range(len(trans_grid_ls)):
         tmp_actor = draw_one_grd_vtk(trans_grid_ls[i])
         tmp_actor.GetProperty().SetOpacity(0.5)
         ren.AddActor(tmp_actor)
@@ -575,7 +575,7 @@ def vis_ested_pcd_corners(ind=1):
             writer.SetFileName("screenshot.png")
             writer.SetInputData(w2if.GetOutput())
             writer.Write()
-            print "screenshot saved"
+            print ("screenshot saved")
 
     style = vtk.vtkInteractorStyleSwitch()
     iren.SetRenderWindow(renWin)
@@ -632,19 +632,98 @@ def find_latest(cali_file_ls):
         number_ls.append(ast.literal_eval(tmp_ls[0] + "." + tmp_ls[1]))
     return cali_file_ls[np.array(number_ls).argmax()]
 
+def calculate_reprojection_error(ind_ls = (np.arange(1, params['poses_num']+1)).tolist()):
+    #load calibration results
+    filels = os.listdir(params['base_dir'])
+    cali_file_ls = []
+    for file in filels:
+        if file.find("cali_result.txt") > -1:
+            cali_file_ls.append(file)
+    if len(cali_file_ls) > 1:
+        warnings.warn("More than one calibration file exit! Load the latest file.", UserWarning)
+        latest_cali = find_latest(cali_file_ls)
+        r_t = np.genfromtxt(os.path.join(params['base_dir'], latest_cali), delimiter=',')
+        print("Load ", latest_cali, " as the extrinsic calibration parameters!")
+
+    #load camera intrinsics
+    intrinsic_paras_tuple = make_tuple(params['instrinsic_para'])
+    intrinsic_paras = np.array(intrinsic_paras_tuple).reshape(3, 3)
+
+    #rotation matrix calculation for point cloud data
+    rot_mat = np.dot(transforms3d.axangles.axangle2mat([0, 0, 1], r_t[2]),
+                     np.dot(transforms3d.axangles.axangle2mat([0, 1, 0], r_t[1]),
+                            transforms3d.axangles.axangle2mat([1, 0, 0], r_t[0])))
+
+
+    #load data (imgs+pcds)
+    error_ls = []
+    ls = ind_ls
+    for i in ls:
+        cords_file = "output/img_corners/" + str(i).zfill(params['file_name_digits']) + "_img_corners.txt"
+        corners_in_img_arr = np.genfromtxt(cords_file, delimiter=",").astype(np.int32)
+        if np.linalg.norm(np.array(corners_in_img_arr[0]) - np.array([0, H])) > np.linalg.norm(
+                np.array(corners_in_img_arr[-1]) - np.array([0, H])):
+            corners_in_img_arr = np.flipud(corners_in_img_arr)
+
+        pcd_result_file = "output/pcd_seg/" + str(i).zfill(params['file_name_digits']) + "_pcd_result.pkl"
+        with open(os.path.abspath(pcd_result_file), "rb") as f:
+            pcd_result_ls = cPickle.load(f)
+        assert pcd_result_ls is not None
+
+        corner_arr = pcd_result_ls[4].reshape(-1, 2)
+        num = corner_arr.shape[0]
+        corner_arr = np.hstack([corner_arr, np.zeros(num).reshape(num, 1)])
+
+        rot1 = pcd_result_ls[0]
+        t1 = pcd_result_ls[1].reshape(1, 3)
+        rot2 = pcd_result_ls[2]
+        t2 = pcd_result_ls[3].reshape(1, 3)
+
+        corners_in_pcd_arr = np.dot(np.dot(rot2.T, corner_arr.T).T - t2 + t1, rot1)
+
+        transformed_pcd = np.dot(rot_mat, corners_in_pcd_arr.T).T + r_t[3:]  # point cloud in the camera coordinate system
+        transformed_pcd_ls = corners_in_pcd_arr.tolist()  # original point cloud in a list
+
+        if params["camera_type"] == "panoramic":
+            pcd2angle_s = map(xyz2angle, transformed_pcd_ls) #calculate pcd points from xyz to polar angles
+            proj_pts = np.array(map(voxel2pixel, pcd2angle_s))
+
+        elif params['camera_type'] == "perspective":
+            cam_coord_pcd = transformed_pcd.copy()
+            pcd_to_pix = (np.dot(intrinsic_paras, cam_coord_pcd.T)).T
+
+        proj_pts = (pcd_to_pix / pcd_to_pix[:, 2].reshape(-1, 1))[:, :2].astype(np.int16)
+        if np.linalg.norm(np.array(proj_pts[0]) - np.array([0, H])) > np.linalg.norm(
+                np.array(proj_pts[-1]) - np.array([0, H])):
+            proj_pts = np.flipud(proj_pts)
+        error = np.mean(np.linalg.norm(corners_in_img_arr-proj_pts, axis=1))
+        error_ls.append(error)
+
+    objects = ls
+    y_pos = np.arange(len(ls))
+    performance = np.array(error_ls)
+
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, ls)
+    plt.ylabel('Pixel')
+    plt.title('Reprojection error')
+    plt.show()
+
+    print("Done")
+
 
 def back_project_pcd(img, pcd_arr, color_arr, r_t, i, hide_occlussion_by_marker):
     # print pcd_arr
     rot_mat = np.dot(transforms3d.axangles.axangle2mat([0, 0, 1], r_t[2]),
                      np.dot(transforms3d.axangles.axangle2mat([0, 1, 0], r_t[1]),
                             transforms3d.axangles.axangle2mat([1, 0, 0], r_t[0])))
-    transformed_pcd = np.dot(rot_mat, pcd_arr.T).T + r_t[3:]
+    transformed_pcd = np.dot(rot_mat, pcd_arr.T).T + r_t[3:] #point cloud in the camera coordinate system
 
-    transformed_pcd_ls = pcd_arr.tolist()
+    transformed_pcd_ls = pcd_arr.tolist() #original point cloud in a list
 
     if not hide_occlussion_by_marker:  # whether remove occlussions by the chessboard
         if params["camera_type"] == "panoramic":
-            pcd2angle_s = map(xyz2angle, transformed_pcd_ls)
+            pcd2angle_s = map(xyz2angle, transformed_pcd_ls) #calculate pcd points from xyz to polar angles
             proj_pts = np.array(map(voxel2pixel, pcd2angle_s))
             point_s = 5
         elif params['camera_type'] == "perspective":
@@ -654,7 +733,7 @@ def back_project_pcd(img, pcd_arr, color_arr, r_t, i, hide_occlussion_by_marker)
 
             # print cam_coord_pcd
 
-            print "before filtering z: ", cam_coord_pcd.shape
+            print ("before filtering z: ", cam_coord_pcd.shape)
             # cam_coord_pcd = cam_coord_pcd[np.where(cam_coord_pcd[:, 2] < 0)]
             # cam_coord_pcd = cam_coord_pcd[:20000, :]
             # print cam_coord_pcd
@@ -663,7 +742,7 @@ def back_project_pcd(img, pcd_arr, color_arr, r_t, i, hide_occlussion_by_marker)
             cam_coord_pcd = cam_coord_pcd[inds]
             color_arr = color_arr[inds]
             # print cam_coord_pcd
-            print "after filtering z: ", cam_coord_pcd.shape
+            print ("after filtering z: ", cam_coord_pcd.shape)
 
             pcd_to_pix = (np.dot(intrinsic_paras, cam_coord_pcd.T)).T
             # pcd_to_pix = pcd_to_pix[np.where(pcd_to_pix[:, 2] > 0)]
@@ -689,7 +768,7 @@ def back_project_pcd(img, pcd_arr, color_arr, r_t, i, hide_occlussion_by_marker)
 
         chessboard_result_file_path = os.path.join(params['base_dir'], "output/pcd_seg/" + str(i).zfill(
             params["file_name_digits"]) + "_pcd_result.pkl")
-        chessboard_result_file = cPickle.load(open(chessboard_result_file_path, "r"))
+        chessboard_result_file = cPickle.load(open(chessboard_result_file_path, "rb"))
         rot1 = chessboard_result_file[0]
         t1 = chessboard_result_file[1].reshape(1, 3)
         # print "rot1*rot1.T: ", np.dot(rot1, rot1.T)
@@ -714,15 +793,15 @@ def back_project_pcd(img, pcd_arr, color_arr, r_t, i, hide_occlussion_by_marker)
 
         # print "before removal: ", transformed_pcd.shape
         inds = remove_occlusion_of_chessboard(transformed_pcd, corners_in_pcd_arr)
-        print "inds:", inds
+        print ("inds:", inds)
         proj_pts = np.array(proj_pcd_2_pix(transformed_pcd))[inds].astype(np.int32)
-        print "after removal: ", proj_pts.shape
+        print ("after removal: ", proj_pts.shape)
         color_arr = color_arr[inds]
 
     print
-    print proj_pts.shape[0], proj_pts.min(axis=0), proj_pts.max(axis=0)
+    print (proj_pts.shape[0], proj_pts.min(axis=0), proj_pts.max(axis=0))
     print
-    for i in xrange(proj_pts.shape[0]):
+    for i in range(proj_pts.shape[0]):
         cv2.circle(img, (proj_pts[i][0], proj_pts[i][1]), point_s, tuple(color_arr[i].tolist()), -1)
 
     return img
@@ -757,19 +836,19 @@ def vis_back_proj(ind=1, img_style="edge", pcd_style="intens", hide_occlussion_b
         warnings.warn("More than one calibration file exit! Load the latest file.", UserWarning)
         latest_cali = find_latest(cali_file_ls)
         r_t = np.genfromtxt(os.path.join(params['base_dir'], latest_cali), delimiter=',')
-        print "Load ", latest_cali, " as the extrinsic calibration parameters!"
+        print ("Load ", latest_cali, " as the extrinsic calibration parameters!")
     elif len(cali_file_ls) == 1:
         r_t = np.genfromtxt(os.path.join(params['base_dir'], cali_file_ls[0]), delimiter=',')
-        print "Load ", cali_file_ls[0], " as the extrinsic calibration parameters!"
+        print ("Load ", cali_file_ls[0], " as the extrinsic calibration parameters!")
     else:
         raise Exception("No calibration file is found!")
 
     if pcd_style == "intens":
         pcd_color = np.fliplr((cm.jet(intens.astype(np.float32) / intens.max()) * 255).astype(np.int32)[:, :3])
     elif pcd_style == "dis":
-        pcd_color = np.fliplr((cm.jet(dis_arr / 10) * 255).astype(np.int32)[:, :3])
+        pcd_color = np.fliplr((cm.jet(dis_arr / 42) * 255).astype(np.int32)[:, :3])
     else:
-        print "Please input the right pcd color style"
+        print ("Please input the right pcd color style")
 
     backproj_img = back_project_pcd(img, pcd, pcd_color, r_t, ind, hide_occlussion_by_marker)
 
@@ -793,14 +872,14 @@ def vis_back_proj(ind=1, img_style="edge", pcd_style="intens", hide_occlussion_b
                                               params["file_name_digits"])) + "_" + img_style + "_" + pcd_style + (
                                  "_hide_occlusion" if hide_occlussion_by_marker else "") + "." + params['image_format']
             cv2.imwrite(save_file_name, img, [cv2.IMWRITE_JPEG_QUALITY, 70])
-            print "The image is saved to ", save_file_name
+            print ("The image is saved to ", save_file_name)
             cv2.destroyAllWindows()
     else:
         save_file_name = os.path.join(params['base_dir'], str(ind).zfill(
             params["file_name_digits"])) + "_" + img_style + "_" + pcd_style + (
                              "_hide_occlusion" if hide_occlussion_by_marker else "") + "." + params['image_format']
         cv2.imwrite(save_file_name, img, [cv2.IMWRITE_JPEG_QUALITY, 70])
-        print "The image is saved to ", save_file_name
+        print ("The image is saved to ", save_file_name)
         cv2.destroyAllWindows()
 
 
